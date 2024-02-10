1d9cd64461e03437bcdf2f35071c9cc726935d38
Greg Zetko <greg.zetko@mail.utoronto.ca>
2024-02-10 14:08:30 -0500

    clear VisionTrackDrive

--- src/main/java/frc/robot/commands/VisionTrackDrive.java
+++ src/main/java/frc/robot/commands/VisionTrackDrive.java
@@ -22,10 +22,6 @@ import frc.robot.Constants.Xbox;
  * Precision/Boost amount can be adjusted through NetworkTables.
  */
 public class VisionTrackDrive extends Command {
-  private final double verticalAngle = 38.0; // limelight angle down
-  private final NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
-  private final NetworkTableEntry txEntry = limelightTable.getEntry("tx");
-  private final NetworkTableEntry tyEntry = limelightTable.getEntry("ty");
   /** Creates a new TeleopHeadingDrive. */
   public VisionTrackDrive() {
     addRequirements(RobotContainer.drive);
@@ -33,42 +29,19 @@ public class VisionTrackDrive extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double tx = txEntry.getDouble(-2609);
-    double ty = tyEntry.getDouble(-2609); // range [-15,20]
-    double forward = (ty+18)/38; // normalize to [0,1]
-    double xSpeed = MathUtil.applyDeadband(-RobotContainer.driverController.getRightTriggerAxis(), Xbox.joystickDeadband)*forward; // "Deadman's switch"
-    if(tx == -2609){
-      DriverStation.reportError("Limelight not connected", null);
-      return;
-    }
-    if(ty == -2609){
-      DriverStation.reportError("Limelight not connected", null);
-      xSpeed = 0;
-    }
-
-    
-    ChassisSpeeds speeds = RobotContainer.drive.drive.swerveController.getTargetSpeeds(
-      // controller is +ve backwards, field coordinates are +ve forward
-      xSpeed,
-      // controller is +ve right, field coordinates are +ve left
-      MathUtil.applyDeadband(-RobotContainer.driverController.getLeftX(), Xbox.joystickDeadband),
-      // controller is +ve right (CW+), YAGSL expects CCW+ (+ve left)
-      RobotContainer.drive.drive.getYaw().getRadians()+Math.toRadians(tx), // -PI to PI radians
-      RobotContainer.drive.drive.getYaw().getRadians(),
-      RobotContainer.drive.getLimitedTeleopLinearSpeed()
-    );
-    RobotContainer.drive.drive.drive(speeds);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println("Vision track done");
+
   }
 
   // Returns true when the command should end.
