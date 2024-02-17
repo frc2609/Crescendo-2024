953bde42575fe65452794f441846c9d476e5ca51
UserC2 <aj01cars@outlook.com>
2024-02-17 11:19:19 -0500

    Add shooter angle and visualizer subsystem and some commands to test them

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,17 +6,23 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.VisionTrackDrive;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.ShooterAngle;
+import frc.robot.utils.Visualizer;
 
 public class RobotContainer {
   public static final Drive drive = new Drive(false);
+  public static final ShooterAngle shooterAngle = new ShooterAngle();
+  public static final Visualizer visualizer = new Visualizer();
+
   public static final CommandXboxController driverController = new CommandXboxController(0);
   private final SendableChooser<Command> autoChooser;
 
@@ -36,9 +42,21 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    driverController.x().onTrue(new InstantCommand(drive.drive::lockPose));
+    // driverController.x().onTrue(new InstantCommand(drive.drive::lockPose));
     driverController.start().onTrue(new InstantCommand(drive.drive::zeroGyro));
-    driverController.y().whileTrue(new VisionTrackDrive());
+    // driverController.y().whileTrue(new VisionTrackDrive());
+    driverController.a().onTrue(new InstantCommand(() -> {
+      shooterAngle.setAngle(Rotation2d.fromDegrees(0));
+    }, shooterAngle));
+    driverController.b().onTrue(new InstantCommand(() -> {
+      shooterAngle.setAngle(Rotation2d.fromDegrees(25));
+    }, shooterAngle));
+    driverController.y().onTrue(new InstantCommand(() -> {
+      shooterAngle.setAngle(Rotation2d.fromDegrees(55));
+    }, shooterAngle));
+    driverController.x().onTrue(new InstantCommand(() -> {
+      shooterAngle.setAngle(Rotation2d.fromDegrees(75));
+    }, shooterAngle));
   }
 
   public Command getAutonomousCommand() {
