0a168e42fe87a4c5fb610e7fa1af80dd64e6094b
UserC2 <aj01cars@outlook.com>
2024-02-24 15:45:15 -0500

    Fix Add Intake Commands #31

---
+++ src/main/java/frc/robot/commands/ExpelNote.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+
+/** Run the intake in reverse (e.g. score on amp or trap). Ends after a timer expires. */
+public class ExpelNote extends Command {
+  /** Creates a new ExpelNote. */
+  public ExpelNote() {
+    addRequirements(RobotContainer.intake);
+    withTimeout(0.4);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.intake.setMotor(0.5);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.intake.setMotor(0);
+  }
+}
