9f87427a4f708c51b54dd4ba06cf2078ec90b2b7
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-03-03 18:24:17 -0500

    Detect alliance colour automatically
    Also:
    * Formatting
    * Put "align speed" entry in "AprilTagAmpAlign" table

--- src/main/java/frc/robot/commands/AprilTagAmpAlign.java
+++ src/main/java/frc/robot/commands/AprilTagAmpAlign.java
@@ -4,43 +4,48 @@
 
 package frc.robot.commands;
 
-import java.util.Optional;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.AprilTag.ID;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Limelight;
 import frc.robot.utils.DriveUtil;
 
+/**
+ * Face intake towards amp and align the robot to the amp with forward/backward velocity.
+ * Left/right (towards/away from amp) velocity is controlled by the driver.
+ * Automatically detects alliance colour.
+ */
 public class AprilTagAmpAlign extends Command {
+  public static final double alignmentSpeedLimit = 2.0;
+  private final PIDController xAlignController = new PIDController(0.5, 0, 0);
+  private ID aprilTagID;
 
-  private final ID aprilTagID = Constants.AprilTag.ID.kRedAmp;;
-
-  PIDController xAlignController = new PIDController(0.5, 0, 0);
-
-  /** Creates a new AmpAlign. */
+  /** Creates a new AprilTagAmpAlign. */
   public AprilTagAmpAlign() {
     addRequirements(RobotContainer.drive);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // update alliance colour each time the command starts (e.g. if changed during testing)
+    // also, if you do it in the constructor and the code starts before the DS or FMS connects, it will grab the default (wrong) alliance
+    aprilTagID = RobotContainer.isRedAlliance("AprilTagAmpAlign") ? ID.kRedAmp : ID.kBlueAmp;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Transform2d targetOffset = RobotContainer.drive.drive.swerveDrivePoseEstimator.getEstimatedPosition().minus(Limelight.getTargetPose2d(aprilTagID));
-    double tagHeading = Limelight.getTargetPose2d(aprilTagID).getRotation().getRadians();
+    Pose2d tagPose = Limelight.getTargetPose2d(aprilTagID);
+    Transform2d targetOffset = RobotContainer.drive.drive.swerveDrivePoseEstimator.getEstimatedPosition().minus(tagPose);
+    double tagHeading = tagPose.getRotation().getRadians();
+
     double[] driverInputs = DriveUtil.getDriverInputs(
       RobotContainer.driverController,
       true,
@@ -50,8 +55,8 @@ public class AprilTagAmpAlign extends Command {
       DriveUtil.getSensitivity(RobotContainer.driverController)
     );
 
-    double calculatedSpeed = MathUtil.clamp(xAlignController.calculate(targetOffset.getY(), 0), -2, 2);
-    SmartDashboard.putNumber("align speed", calculatedSpeed);
+    double calculatedSpeed = MathUtil.clamp(xAlignController.calculate(targetOffset.getY(), 0), -alignmentSpeedLimit, alignmentSpeedLimit);
+    SmartDashboard.putNumber("AprilTagAmpAlign/Alignment Speed (mps)", calculatedSpeed);
 
     ChassisSpeeds speeds = RobotContainer.drive.drive.swerveController.getTargetSpeeds(
       calculatedSpeed,
@@ -62,7 +67,6 @@ public class AprilTagAmpAlign extends Command {
     );
 
     RobotContainer.drive.drive.driveFieldOriented(speeds);
-
   }
 
   // Called once the command ends or is interrupted.
