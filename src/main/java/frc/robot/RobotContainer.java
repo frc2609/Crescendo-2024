f96c9d19af250444b956d6afee611b0b02ba4b00
UserC2 <aj01cars@outlook.com>
2024-03-07 21:55:51 -0500

    Add shooter controls and intake controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,11 +8,14 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.util.PathPlannerLogging;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.AprilTag.ID;
 import frc.robot.commands.AprilTagTrackDrive;
@@ -25,6 +28,7 @@ import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.ShooterAngle;
 import frc.robot.subsystems.ShooterFlywheel;
 // import frc.robot.utils.Visualizer;
+import frc.robot.subsystems.ShooterFlywheel.SpinType;
 
 public class RobotContainer {
   public static final Drive drive = new Drive(false);
@@ -65,8 +69,8 @@ public class RobotContainer {
     // TODO: Move these to 'Test' mode as applicable
 
     // Swerve
-    driverController.start().onTrue(new InstantCommand(drive.drive::zeroGyro));
-    driverController.y().whileTrue(new AprilTagTrackDrive(true, ID.kRedSpeakerCenter));
+    // driverController.start().onTrue(new InstantCommand(drive.drive::zeroGyro));
+    // driverController.y().whileTrue(new AprilTagTrackDrive(true, ID.kRedSpeakerCenter));
     
     // Elevator
     driverController.povUp().onTrue(new MoveElevatorToPosition(Position.trap));
@@ -76,15 +80,32 @@ public class RobotContainer {
     // Intake
     // Fake the note being picked up during simulation.
     // Doesn't require intake so intake commands aren't cancelled when run.
-    driverController.back().onTrue(new InstantCommand(() -> intake.noteHeld = true));
-    // driverController.a().onTrue(intake.getIntakeNote());
-    // driverController.b().onTrue(intake.getExpelNote());
-    // driverController.y().onTrue(intake.getFeedNote());
-    // driverController.x().onTrue(intake.getTurnOff());
+    // driverController.back().onTrue(new InstantCommand(() -> intake.noteHeld = true));
+    driverController.a().onTrue(intake.getIntakeNote());
+    driverController.b().onTrue(intake.getExpelNote());
+    driverController.y().onTrue(intake.getFeedNote());
+    driverController.x().onTrue(intake.getTurnOff());
     
     // Shooter Angle
     
     // Shooter Flywheel
+
+    // Shooter Angle
+    driverController.start().toggleOnTrue(
+      new RunCommand(() -> RobotContainer.shooterAngle.setAngle(Rotation2d.fromDegrees(SmartDashboard.getNumber("Test/Shooter Target Angle (Deg)", 0))), RobotContainer.shooterAngle)
+    );
+
+    // Shooter Flywheel
+    driverController.back().toggleOnTrue(
+      new RunCommand(() -> {
+        SpinType spinType = SpinType.disable;
+        if (RobotContainer.driverController.leftBumper().getAsBoolean())
+          spinType = SpinType.slowLeftMotor;
+        if (RobotContainer.driverController.rightBumper().getAsBoolean())
+          spinType = SpinType.slowRightMotor;
+        RobotContainer.shooterFlywheel.setSpeed(SmartDashboard.getNumber("Test/Shooter Target RPM", 0), spinType);
+      }, RobotContainer.shooterFlywheel).finallyDo(() -> RobotContainer.shooterFlywheel.setSpeed(0, SpinType.disable))
+    );
   }
 
   public Command getAutonomousCommand() {
