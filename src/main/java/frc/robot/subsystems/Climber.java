2404dddd652693de6b2036f2d63c981f77e6144a
UserC2 <aj01cars@outlook.com>
2024-03-11 16:40:13 -0400

    Improve formatting

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -11,9 +11,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
@@ -21,13 +19,13 @@ import frc.robot.RobotContainer;
 public class Climber extends SubsystemBase {
   private final CANSparkMax climberMotor = new CANSparkMax(16, MotorType.kBrushless);
   private final SparkPIDController climberPID;
-  double climbpos = 0.0;
 
   public Climber() {
     climberMotor.restoreFactoryDefaults();
     climberMotor.setIdleMode(IdleMode.kBrake);
     climberMotor.setInverted(false);
     climberMotor.setSmartCurrentLimit(40);
+
     climberPID = climberMotor.getPIDController();
     climberPID.setSmartMotionMaxVelocity(5000, 0);
     climberPID.setSmartMotionMinOutputVelocity(0, 0);
@@ -39,33 +37,23 @@ public class Climber extends SubsystemBase {
     climberPID.setIZone(0.3);
     climberPID.setFF(0.0002);
     climberPID.setOutputRange(-1, 1);
-    
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("CLIMBER", climberMotor.getEncoder().getPosition());
-    SmartDashboard.putNumber("CLIMBER 2", climberMotor.getAppliedOutput());
+    SmartDashboard.putNumber("Climber/Position (Rotations)", climberMotor.getEncoder().getPosition());
+    SmartDashboard.putNumber("Climber/Applied Output (-1-1)", climberMotor.getAppliedOutput());
   }
 
-  @Override
-  public void simulationPeriodic() {}
-
-
   public Command Raise() {
-    return new RunCommand(() -> climberMotor.set(RobotContainer.driverController.getRawAxis(2)));
+    return new RunCommand(() -> climberMotor.set(RobotContainer.driverController.getLeftTriggerAxis()), this);
   }
 
   public Command Lower() {
-    return new RunCommand(() -> climberMotor.set(-RobotContainer.driverController.getRawAxis(3)));
+    return new RunCommand(() -> climberMotor.set(-RobotContainer.driverController.getRightTriggerAxis()), this);
   }
 
   public Command Stop() {
-    return new InstantCommand(() -> climbpos = climberMotor.getEncoder().getPosition()).andThen(() ->  climberPID.setReference(climbpos, CANSparkMax.ControlType.kSmartMotion));
+    return new InstantCommand(() -> climberPID.setReference(climberMotor.getEncoder().getPosition(), CANSparkMax.ControlType.kSmartMotion), this);
   }
-
-  public Command ResetEnc() {
-    return new InstantCommand(() -> climberMotor.getEncoder().setPosition(1.0));
-  }
-
 }
