ec56cdcfe305ad2b2e6d1ffc1cf5f03a2e75ad90
UserC2 <aj01cars@outlook.com>
2024-03-11 18:42:38 -0400

    Modify commands to use updated Shooter Angle subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -54,7 +54,6 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {
     TunableNumber.updateAll();
-    RobotContainer.shooterAngle.anglePID.reset(RobotContainer.shooterAngle.getAngle().getDegrees());
   }
 
   @Override
