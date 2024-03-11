5615884d8a628b976bb5de46f0d0a18397992a57
UserC2 <aj01cars@outlook.com>
2024-03-10 20:25:53 -0400

    Reenable zero-gyro command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.MoveElevatorToPosition;
@@ -65,7 +66,7 @@ public class RobotContainer {
     // TODO: Move these to 'Test' mode as applicable
 
     // Swerve
-    // driverController.start().onTrue(new InstantCommand(drive.drive::zeroGyro));
+    driverController.start().onTrue(new InstantCommand(drive.drive::zeroGyro));
     // driverController.y().whileTrue(AprilTagTrackDrive.getAlignToSpeaker(true));
     // driverController.a().whileTrue(new AprilTagAmpAlign());
 
