a690d38057741258d70b6db76dee36e302329623
UserC2 <aj01cars@outlook.com>
2024-03-13 21:17:50 -0400

    Adjust amp height

--- src/main/java/frc/robot/commands/AutoScoreAmp.java
+++ src/main/java/frc/robot/commands/AutoScoreAmp.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
@@ -31,6 +32,7 @@ public class AutoScoreAmp extends SequentialCommandGroup {
         )
       ),
       RobotContainer.intake.getExpelNote(),
+      Commands.waitSeconds(0.25),
       new ResetIntakeAndElevator()
     );
   }
