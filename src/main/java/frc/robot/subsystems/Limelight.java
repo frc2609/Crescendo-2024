562c4e005864ebd93bdbd416a5e78ca8b03498b1
UserC2 <aj01cars@outlook.com>
2024-03-14 13:38:38 -0400

    Revert 3a809439448a09733e4ac346e8c5fbcf0001b873 because it doesn't set robot pose

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -9,44 +9,32 @@ import java.util.Optional;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.utils.LimeLightHelpers;
 import frc.robot.utils.LimeLightHelpers.LimelightTarget_Fiducial;
+import frc.robot.utils.LimeLightHelpers.Results;
 
 public class Limelight extends SubsystemBase {
   public static Pose2d limelightPose = new Pose2d();
-  private static NetworkTable limelightTable;
 
   /** Creates a new Limelight. */
-  public Limelight() {
-    limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
-  }
+  public Limelight() {}
 
   @Override
   public void periodic() {
     // Only consider updating odometry if the reading from limelight is valid
     // False when no valid tags detected
-    boolean seesValidTags = limelightTable.getEntry("tv").getBoolean(false);
-    if (seesValidTags) {
+    if (LimeLightHelpers.getTV("limelight")) {
+      Results results = LimeLightHelpers.getLatestResults("limelight").targetingResults;
 
-      // Contains X,Y,Z, Roll,Pitch,Yaw, total latency (cl + tl), tag count, tag span, avg distance of tag from camera, average tag area (% of image)
-      double[] poseArray = limelightTable.getEntry("botpose_wpiblue").getDoubleArray(new double[11]);
-      
       // Record recently detected pose
-      limelightPose = new Pose2d(poseArray[0], poseArray[1], new Rotation2d(poseArray[5]));
-
-      // get percentage
-      double totalArea = limelightTable.getEntry("ta").getDouble(0);
-
-      double latency = poseArray[6];
+      limelightPose = results.getBotPose2d_wpiBlue();
 
       // Calculate the vector between the current drivetrain pose and vision pose
       Transform2d odometryDifference = RobotContainer.drive.drive.field.getRobotPose().minus(limelightPose);
@@ -59,18 +47,18 @@ public class Limelight extends SubsystemBase {
       double xyStds;
       double degStds;
       
-      int numTargets = (int) poseArray[7];
-      SmartDashboard.putNumber("Limelight/Target Area", totalArea);
+      int numTargets = results.targets_Fiducials.length;
+      SmartDashboard.putNumber("Target area", getBestTargetArea(results.targets_Fiducials));
       
       if (numTargets >= 2) {
         // trust vision odometry more if we see more than one apriltag
         // orientation should also be more accurate in this case
         xyStds = 0.5;
         degStds = 6;
-      } else if (totalArea > 0.8 && distance < 0.5) {
+      } else if (LimeLightHelpers.getTA("limelight") > 0.8 && distance < 0.5) {
         xyStds = 1.0;
         degStds = 12;
-      } else if (totalArea > 0.1 && distance < 0.3) {
+      } else if (LimeLightHelpers.getTA("limelight") > 0.1 && distance < 0.3) {
         xyStds = 2.0;
         degStds = 30;
       } else {
@@ -78,12 +66,15 @@ public class Limelight extends SubsystemBase {
         return;
       }
 
+      // crash the code so I can see why it went wrong (makes it nice and detectable)
+      if (!results.valid) throw new RuntimeException("Limelight results invalid after validity check!");
+
       // set the "trust factor" of the vision measurement
       RobotContainer.drive.drive.swerveDrivePoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(xyStds, xyStds, Units.degreesToRadians(degStds)));
       
       if (distance > 0.02 && RobotContainer.drive.getVelocity() < 0.05) {
         // add a vision measurement if the cartesian error in odometry is greater than 0.02m
-        RobotContainer.drive.drive.swerveDrivePoseEstimator.addVisionMeasurement(limelightPose, Timer.getFPGATimestamp() - (latency/1000.0));
+        RobotContainer.drive.drive.swerveDrivePoseEstimator.addVisionMeasurement(limelightPose, Timer.getFPGATimestamp() - (results.latency_capture/1000.0));
       }
     }
 
