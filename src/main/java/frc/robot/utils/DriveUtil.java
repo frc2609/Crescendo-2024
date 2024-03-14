c501fa98c5102727bcbdbeafcee553056e00b5b5
UserC2 <aj01cars@outlook.com>
2024-03-14 17:12:39 -0400

    Remove precision controls (not used by driver)

--- src/main/java/frc/robot/utils/DriveUtil.java
+++ src/main/java/frc/robot/utils/DriveUtil.java
@@ -34,7 +34,7 @@ public class DriveUtil {
    */
   public static double getSensitivity(CommandXboxController controller) {
     // define sensitivity calculations here, that way if they change, they will change everywhere
-    boolean precision = controller.getHID().getLeftBumper() || controller.getHID().getRightBumper();
+    boolean precision = false; //controller.getHID().getLeftBumper() || controller.getHID().getRightBumper();
     return precision ? 0.5 : 1.0;
   }
 
