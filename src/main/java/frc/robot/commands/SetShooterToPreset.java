e33af99a1daa7912c6076f885df504a547a47489
UserC2 <aj01cars@outlook.com>
2024-03-15 21:23:31 -0400

    Add option to reset odometry with shooter presets

--- src/main/java/frc/robot/commands/SetShooterToPreset.java
+++ src/main/java/frc/robot/commands/SetShooterToPreset.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
@@ -14,33 +15,53 @@ import frc.robot.subsystems.ShooterFlywheel.SpinType;
  */
 public class SetShooterToPreset extends Command {
   public enum ShooterPreset {
-    // TODO: placeholders; tune
-    kAtSpeaker(3000.0, 55.0, SpinType.disable),
-    kAtPodium(4000.0, 35.0, SpinType.slowLeftMotor),
-    kThrowNote(5800.0, 10.0, SpinType.slowLeftMotor);
+    kAtSpeaker(3000.0, 55.0, SpinType.slowRightMotor,
+      new Pose2d(15.1, 5.5, Rotation2d.fromDegrees(0)),
+      new Pose2d(1.4, 5.5, Rotation2d.fromDegrees(0))
+    ),
+    kAtPodium(4000.0, 35.0, SpinType.slowRightMotor,
+      new Pose2d(13.8, 4.11, Rotation2d.fromDegrees(0)),
+      new Pose2d(2.69, 4.11, Rotation2d.fromDegrees(0))
+    ),
+    kThrowNote(5800.0, 20.0, SpinType.slowRightMotor, new Pose2d(), new Pose2d());
 
     public final Rotation2d angle;
     public final double RPM;
     public final SpinType spinType;
+    public final Pose2d redPose;
+    public final Pose2d bluePose;
 
-    private ShooterPreset(double RPM, double angleDeg, SpinType spinType) {
+    private ShooterPreset(double RPM, double angleDeg, SpinType spinType, Pose2d redPose, Pose2d bluePose) {
       this.RPM = RPM;
       this.angle = Rotation2d.fromDegrees(angleDeg);
       this.spinType = spinType;
+      this.redPose = redPose;
+      this.bluePose = bluePose;
     }
   }
 
   private final ShooterPreset preset;
+  private final boolean resetOdometry;
 
   /** Creates a new SetShooterToPreset. */
-  public SetShooterToPreset(ShooterPreset preset) {
+  public SetShooterToPreset(ShooterPreset preset, boolean resetOdometry) {
     this.preset = preset;
+    this.resetOdometry = resetOdometry;
     addRequirements(RobotContainer.shooterAngle, RobotContainer.shooterFlywheel);
   }
 
   @Override
   public void initialize() {
+    if (resetOdometry) {
+      RobotContainer.drive.drive.resetOdometry(RobotContainer.isRedAlliance("SetShooterToPreset") ? preset.redPose : preset.bluePose);
+    }
     RobotContainer.shooterAngle.setAngle(preset.angle);
     RobotContainer.shooterFlywheel.setSpeed(preset.RPM, preset.spinType);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.shooterAngle.stop();
+    RobotContainer.shooterFlywheel.coast();
   }
 }
