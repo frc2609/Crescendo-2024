52c7c24412689efac07422d810e3810a635dc030
UserC2 <aj01cars@outlook.com>
2024-03-16 20:58:00 -0400

    Height offset is now adjustable, and set height offset such that shooter actually functions

--- src/main/java/frc/robot/commands/AutoSetShooter.java
+++ src/main/java/frc/robot/commands/AutoSetShooter.java
@@ -23,10 +23,10 @@ import frc.robot.subsystems.ShooterFlywheel.SpinType;
 public class AutoSetShooter extends Command {
   // Units in meters, positive = up or forward
   public static final double speakerHeight = 1.98;
-  public static final double heightOffset = 0.425;
+  public static double heightOffset = 1.2;
   public static final double noteHeight = 0.19; // distance from ground to note at shooter pivot
   public static final double shooterDistanceFromCenter = -0.02;
-  public static final double targetHeight = speakerHeight + heightOffset - noteHeight;
+  public static double targetHeight = speakerHeight + heightOffset - noteHeight;
 
   // generate a linear equation that passes through these two points
   // distance for RPM is measured from the center of the robot
@@ -45,6 +45,7 @@ public class AutoSetShooter extends Command {
   public AutoSetShooter(SpinType spinType) {
     this.spinType = spinType;
     addRequirements(RobotContainer.shooterAngle, RobotContainer.shooterFlywheel);
+    SmartDashboard.putNumber("Commands/AutoSetShooter/Height Offset", heightOffset);
   }
 
   @Override
@@ -59,6 +60,8 @@ public class AutoSetShooter extends Command {
     final Pose2d robotPose = RobotContainer.drive.drive.getPose();
     final double distanceToSpeaker = robotPose.getTranslation().getDistance(speakerTranslation);
     final double pivotDistanceToSpeaker = distanceToSpeaker + shooterDistanceFromCenter;
+    heightOffset = SmartDashboard.getNumber("Commands/AutoSetShooter/Height Offset", heightOffset);
+    targetHeight = speakerHeight + heightOffset - noteHeight;
     
     final Rotation2d angle = Rotation2d.fromRadians(Math.atan(targetHeight / pivotDistanceToSpeaker));
     final double rpm = rpmEquationSlope * (distanceToSpeaker - closeDistance) + closeRPM;
