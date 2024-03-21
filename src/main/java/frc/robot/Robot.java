a79d3ca5ad88acf8aa4952e4d225286c13b8e76a
UserC2 <aj01cars@outlook.com>
2024-03-21 15:05:39 -0400

    Revert "Mark out questionable line of code"
    This reverts commit a92373da21c82198924ed754af37646fddabb6d9.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,7 +47,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
-    RobotContainer.rearLimelight.updateOdometry();
     CommandScheduler.getInstance().run();
     RobotContainer.visualizer.update();
   }
@@ -81,9 +80,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-    // yeah so that doesn't actually do anything
-    // nice
-    // fyi schedule the command, this does nothing except create a new command during autonomous
     RobotContainer.rearLimelight.getEstimateRobotPose(true);
   }
 
