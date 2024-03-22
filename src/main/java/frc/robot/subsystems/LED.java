1314b4016f5ef3a2d18450f169a5cd59a71da25b
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-03-22 19:52:54 -0400

    Formatting

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -14,15 +14,10 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
-/** Add your docs here. */
 public class LED extends SubsystemBase {
-  // REV Blinkin pretends to be a PWM motor controller
-  // private static final AddressableLED controller = new AddressableLED(2);
   private static Pattern pattern_drive;
   private static Pattern pattern_human;
   private static BlinkMode blinkMode_drive;
@@ -32,14 +27,15 @@ public class LED extends SubsystemBase {
   private static int blinking_i = 0;
   private static int blinking_i_human = 0;
 
-  public enum BlinkMode{
+  public enum BlinkMode {
     SOLID,
     BLINKING_ON,
     BLINKING_OFF,
     FIRE,
     OFF;
   };
-  public enum Pattern{
+
+  public enum Pattern {
     INTAKE_NO_NOTE,
     INTAKE_NOTE,
     INTAKE_IDLE,
@@ -47,18 +43,19 @@ public class LED extends SubsystemBase {
     FIRE
   };
 
-  public static final Map<Pattern, Color> PATTERN_MAP = new HashMap<Pattern, Color>(){{
-      put(Pattern.INTAKE_NO_NOTE, new Color(50, 30, 0));
-      put(Pattern.INTAKE_NOTE, new Color(0, 100, 0));
-      put(Pattern.INTAKE_IDLE, new Color(50, 0, 50));
-      put(Pattern.RED, new Color(50, 0, 0));
+  public static final Map<Pattern, Color> PATTERN_MAP = new HashMap<Pattern, Color>() {{
+    put(Pattern.INTAKE_NO_NOTE, new Color(50, 30, 0));
+    put(Pattern.INTAKE_NOTE, new Color(0, 100, 0));
+    put(Pattern.INTAKE_IDLE, new Color(50, 0, 50));
+    put(Pattern.RED, new Color(50, 0, 0));
   }};
+
   int DRIVE_START = 0;
   int DRIVE_END = 46;
   final int HUMAN_START = 47;
   final int HUMAN_END = 91;
   
-  public LED(){
+  public LED() {
     pattern_drive = Pattern.INTAKE_IDLE;
     pattern_human = Pattern.INTAKE_IDLE;
     blinkMode_drive = BlinkMode.SOLID;
@@ -73,6 +70,13 @@ public class LED extends SubsystemBase {
     led_dev.setData(led);
     led_dev.start();
   }
+
+  @Override
+  public void periodic() {
+    setBuffer();
+    led_dev.setData(led);
+  }
+
   public Color getMovingFireColor(int position, int totalLEDs) {
     // Calculate offset based on time for movement
     double time = Timer.getFPGATimestamp();
@@ -88,107 +92,101 @@ public class LED extends SubsystemBase {
     } else {
       return interpolateColor(new Color(80, 20, 0), new Color(100, 0, 0), (ratio - 0.5) / 0.5);
     }
-}
-private Color interpolateColor(Color startColor, Color endColor, double ratio) {
-  int red = (doubleToInt(startColor.red) + (int)(ratio * (doubleToInt(endColor.red) - doubleToInt(startColor.red))));
-  int green = (doubleToInt(startColor.green) + (int)(ratio * (doubleToInt(endColor.green) - doubleToInt(startColor.green))));
-  int blue = (doubleToInt(startColor.blue) + (int)(ratio * (doubleToInt(endColor.blue) - doubleToInt(startColor.blue))));
-  return new Color(red, green, blue);
-}
-
-// Maps doubles from [0,1] to integers in [0,255]
-private int doubleToInt(double zeroToOne){
-  return (int)(zeroToOne*255.0);
-}
-  @Override
-  public void periodic(){
-    
-    setBuffer();
-    led_dev.setData(led);
   }
 
-  public void setBuffer(){
+  private Color interpolateColor(Color startColor, Color endColor, double ratio) {
+    int red = (doubleToInt(startColor.red) + (int)(ratio * (doubleToInt(endColor.red) - doubleToInt(startColor.red))));
+    int green = (doubleToInt(startColor.green) + (int)(ratio * (doubleToInt(endColor.green) - doubleToInt(startColor.green))));
+    int blue = (doubleToInt(startColor.blue) + (int)(ratio * (doubleToInt(endColor.blue) - doubleToInt(startColor.blue))));
+    return new Color(red, green, blue);
+  }
+
+  // Maps doubles from [0,1] to integers in [0,255]
+  private int doubleToInt(double zeroToOne) {
+    return (int)(zeroToOne*255.0);
+  }
+
+  public void setBuffer() {
     Color color = new Color(0, 0, 0);
-    for(int i = DRIVE_START; i < DRIVE_END; i++){
+    for (int i = DRIVE_START; i < DRIVE_END; i++) {
       led.setLED(i, color);
     }
-    switch(blinkMode_drive){
+    switch (blinkMode_drive) {
       case BLINKING_OFF:
-        for(int i = DRIVE_START; i < DRIVE_END; i++){
+        for (int i = DRIVE_START; i < DRIVE_END; i++) {
           led.setLED(i, color);
         }
-        if(blinking_i < 2){
+        if (blinking_i < 2) {
           blinking_i++;
-        }else{
+        } else {
           blinking_i = 0;
           blinkMode_drive = BlinkMode.BLINKING_ON;
         }
         break;
       case BLINKING_ON:
         color = PATTERN_MAP.getOrDefault(pattern_drive, new Color(0, 0, 0));
-        for(int i = DRIVE_START; i < DRIVE_END; i++){
+        for (int i = DRIVE_START; i < DRIVE_END; i++) {
           led.setLED(i, color);
         }
-        if(blinking_i < 2){
+        if (blinking_i < 2) {
           blinking_i++;
-        }else{
+        } else {
           blinking_i = 0;
           blinkMode_drive = BlinkMode.BLINKING_OFF;
         }
         break;
       case SOLID:
         color = PATTERN_MAP.getOrDefault(pattern_drive, new Color(0, 0, 0));
-        for(int i = DRIVE_START; i < DRIVE_END; i++){
+        for (int i = DRIVE_START; i < DRIVE_END; i++) {
           led.setLED(i, color);
         }
         break;
       case OFF:
         color = new Color(0, 0, 0);
-        for(int i = DRIVE_START; i < DRIVE_END; i++){
+        for (int i = DRIVE_START; i < DRIVE_END; i++) {
           led.setLED(i, color);
         }
         break;
       default:
         color = new Color(10, 10, 10);
-        for(int i = DRIVE_START; i < DRIVE_END; i++){
+        for (int i = DRIVE_START; i < DRIVE_END; i++) {
           led.setLED(i, color);
         }
         DriverStation.reportError("INVALID LED STATE", null);
     }
     
     color = new Color(0, 0, 0);
-    switch(blinkMode_human){
+    switch (blinkMode_human) {
       case OFF:
-        for(int i = HUMAN_START; i < HUMAN_END; i++){
+        for (int i = HUMAN_START; i < HUMAN_END; i++) {
           led.setLED(i, color);
         }
         break;
       case SOLID:
         color = PATTERN_MAP.getOrDefault(pattern_human, new Color(0, 0, 0));
-
-        for(int i = HUMAN_START; i < HUMAN_END; i++){
+        for (int i = HUMAN_START; i < HUMAN_END; i++) {
           led.setLED(i, color);
         }
         break;
       case BLINKING_OFF:
-      for(int i = HUMAN_START; i < HUMAN_END; i++){
-        led.setLED(i, color);
-      }
-        if(blinking_i_human < 2){
+        for (int i = HUMAN_START; i < HUMAN_END; i++) {
+          led.setLED(i, color);
+        }
+        if (blinking_i_human < 2) {
           blinking_i_human++;
-        }else{
+        } else {
           blinking_i_human = 0;
           blinkMode_human = BlinkMode.BLINKING_ON;
         }
         break;
       case BLINKING_ON:
         color = PATTERN_MAP.getOrDefault(pattern_human, new Color(0, 0, 0));
-        for(int i = HUMAN_START; i < HUMAN_END; i++){
+        for (int i = HUMAN_START; i < HUMAN_END; i++) {
           led.setLED(i, color);
         }
-        if(blinking_i_human < 2){
+        if (blinking_i_human < 2) {
           blinking_i_human++;
-        }else{
+        } else { 
           blinking_i_human = 0;
           blinkMode_human = BlinkMode.BLINKING_OFF;
         }
@@ -196,15 +194,14 @@ private int doubleToInt(double zeroToOne){
       case FIRE:
         color = new Color(0, 0, 0);
         int length = HUMAN_END-HUMAN_START;
-        for(int i = HUMAN_START; i < HUMAN_END; i++){
-          color = getMovingFireColor(i-length,length);
+        for (int i = HUMAN_START; i < HUMAN_END; i++) {
+          color = getMovingFireColor(i - length, length);
           led.setLED(i, color);
         }
         break;
     }
   }
 
-  /** Colour values are located in {@link frc.robot.Constants.LED Constants::LED}. */
   public void setDrive(Pattern pattern, BlinkMode blink) {
     pattern_drive = pattern;
     blinkMode_drive = blink;
