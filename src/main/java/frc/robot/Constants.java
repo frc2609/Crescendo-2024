9ce28482207fdf50ed6ace6dd695e7a04a67c014
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-03-30 18:03:44 -0400

    Fudge wheel diameter based on odometry vs. actual distance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -75,7 +75,7 @@ public final class Constants {
     /** The maximum possible RPM of a Kraken X60 with FOC disabled. */
     public static final double maxDriveRPM = 4350;
     /** Make sure to adjust this as the wheels wear. */
-    public static final double wheelDiameter = Units.inchesToMeters(3.75);
+    public static final double wheelDiameter = Units.inchesToMeters(3.75) * (2.95 / 3.0);
     // 5 : 1 drives 4 : 1 ultraplanetary attached to 22T pulley spinning 64T module pulley
     public static final double angleGearRatio = (64.0 / 22.0) * UltraPlanetaryRatios.fiveToOne * UltraPlanetaryRatios.fourToOne;
     // 15 : 24 gearing connected to 3 : 1 bevel gears
