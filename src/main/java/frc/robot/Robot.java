9e584a39ab4ee6c273fa77f324078a6d37597e32
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-03-30 21:52:03 -0400

    Create Limelight estimated pose objects on field in Limelight() instead of robotInit() * Is this really necessary? I don't think these cause that much of a delay...

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,8 +47,6 @@ public class Robot extends TimedRobot {
     robotContainer = new RobotContainer();
     RobotContainer.led.setDrive(Pattern.INTAKE_IDLE, BlinkMode.SOLID);
     RobotContainer.led.setHuman(Pattern.FIRE, BlinkMode.FIRE);
-    RobotContainer.drive.drive.field.getObject("limelight Estimated Pose");
-    RobotContainer.drive.drive.field.getObject("limelight-shooter Estimated Pose");
 
     // preload PathPlanner follow path command so it doesn't delay autonomous
     FollowPathCommand.warmupCommand().schedule();
